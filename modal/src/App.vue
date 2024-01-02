38ba27838406569a6892f8cbf11f0e95131aef2f
ssb6165 <ssb6165@gmail.com>
2024-01-02 15:26:31 +0900

    new file:   modal/.gitignore 	new file:   modal/README.md 	new file:   modal/babel.config.js 	new file:   modal/jsconfig.json 	new file:   modal/package-lock.json 	new file:   modal/package.json 	new file:   modal/public/favicon.ico 	new file:   modal/public/index.html 	new file:   modal/src/App.vue 	new file:   modal/src/assets/logo.png 	new file:   modal/src/components/HelloWorld.vue 	new file:   modal/src/main.js 	new file:   modal/src/tModal.vue 	new file:   modal/vue.config.js

---
+++ modal/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Modal from './tModal.vue'
+import { ref } from 'vue'
+
+const showModal = ref(false)
+</script>
+
+<template>
+  <button id="show-modal" @click="showModal = true"> 모달 보여주기 </button> 
+
+  <Teleport to="body">
+    <modal :show="showModal" @close="showModal = false">
+     <template #header>
+        <h3> 커스텀 header</h3>
+      </template>
+
+      <template #body>
+        커스텀 바디
+      </template>
+    </modal>
+  </Teleport>
+</template>
